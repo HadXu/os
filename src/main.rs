c64312513fe974bc7799ceb787bd5574d4dea0a9
hadxu <hadxu123@gmail.com>
2021-05-18 16:33:53 +0800

    fix chapter-06 double fault panic

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,10 @@ pub extern "C" fn _start() -> ! {
     println!("Hello World.........{}", "!");
 
     os::init();
-    x86_64::instructions::interrupts::int3();
+    // x86_64::instructions::interrupts::int3();
+    unsafe {
+        *(0xdeadbeef as *mut u64) = 42;
+    };
 
     println!("It did not crash!");
 
