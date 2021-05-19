588f7db15afdafb305563b8cc0d5eeb67474f1dc
hadxu <hadxu123@gmail.com>
2021-05-19 14:48:12 +0800

    finish physical memory alloc

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use os::memory::translate_addr;
-use os::{print, println};
+use os::memory::BootInfoFrameAllocator;
+use os::{memory, print, println};
 
 entry_point!(kernel_main);
 
@@ -19,25 +20,38 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World.........{}", "!");
     os::init();
 
-    use x86_64::VirtAddr;
+    use x86_64::structures::paging::Page;
+    use x86_64::{structures::paging::Translate, VirtAddr};
+
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
 
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
-        println!("{:?} -> {:?}", virt, phys);
-    }
+    // let page = Page::containing_address(VirtAddr::new(0xbeaf000));
+    // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+    // let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    // unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e)};
+
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let mapper = unsafe { memory::init(phys_mem_offset) };
+
+    // let addresses = [
+    //     // the identity-mapped vga buffer page
+    //     0xb8000,
+    //     // some code page
+    //     0x201008,
+    //     // some stack page
+    //     0x0100_0020_1a10,
+    //     // virtual address mapped to physical address 0
+    //     boot_info.physical_memory_offset,
+    // ];
+
+    // for &address in &addresses {
+    //     let virt = VirtAddr::new(address);
+    //     let phys = mapper.translate_addr(virt);
+    //     println!("{:?} -> {:?}", virt, phys);
+    // }
 
     // use os::memory::active_level_4_table;
     // use x86_64::structures::paging::PageTable;
