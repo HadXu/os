f6eb9c9def82ed81afae6c05dcbb5cb607b20ee9
hadxu <hadxu123@gmail.com>
2021-05-19 15:57:38 +0800

    update heap malloc

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,16 @@
 
 // mod vga_buffer;
 
+extern crate alloc;
+
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use os::memory::translate_addr;
 use os::memory::BootInfoFrameAllocator;
-use os::{memory, print, println};
+use os::{allocator, memory, print, println};
+
+use x86_64::{structures::paging::Translate, VirtAddr};
 
 entry_point!(kernel_main);
 
@@ -20,13 +25,42 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World.........{}", "!");
     os::init();
 
-    use x86_64::structures::paging::Page;
-    use x86_64::{structures::paging::Translate, VirtAddr};
-
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
-
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialization failed");
+
+    let heap_value = Box::new(41);
+    println!("heap_value at {:p}", heap_value);
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
+
+    // use x86_64::structures::paging::Page;
+    // use x86_64::{structures::paging::Translate, VirtAddr};
+
+    // let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    // let mut mapper = unsafe { memory::init(phys_mem_offset) };
+
+    // let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     // let page = Page::containing_address(VirtAddr::new(0xbeaf000));
     // memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
